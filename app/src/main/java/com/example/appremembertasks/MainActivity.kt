419510db9c12c7cc87987d5a9d2c680cc67fafe9
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2021-07-26 21:42:17 -0300

    completed project
    create datasource in memory cash, create view empty when without tasks.

--- app/src/main/java/com/example/appremembertasks/MainActivity.kt
+++ app/src/main/java/com/example/appremembertasks/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.appremembertasks
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.appremembertasks.dataSource.TaskDataSource
+import com.example.appremembertasks.databinding.ActivityMainBinding
+import com.example.appremembertasks.ui.AddTaskActivity
+import com.example.appremembertasks.ui.TaskListAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
+        updateList()
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+        }
+        adapter.listenerEdit = {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            startActivityForResult(intent, CREATE_NEW_TASK)
+        }
+
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK) updateList()
+    }
+
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
+    }
+
+    companion object {
+        private const val CREATE_NEW_TASK = 1000
     }
 }
--- app/src/main/java/com/example/appremembertasks/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/appremembertasks/ui/AddTaskActivity.kt
@@ -1,12 +1,18 @@
 package com.example.appremembertasks.ui
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.appremembertasks.dataSource.TaskDataSource
 import com.example.appremembertasks.databinding.ActivityAddTaskBinding
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+
 import java.util.*
 import com.example.appremembertasks.extensions.format
 import com.example.appremembertasks.extensions.text
+import com.example.appremembertasks.model.Task
 
 class AddTaskActivity : AppCompatActivity() {
 
@@ -17,6 +23,15 @@ class AddTaskActivity : AppCompatActivity() {
 
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (intent.hasExtra(TASK_ID)) {
+            val taskId = intent.getIntExtra(TASK_ID, 0)
+            TaskDataSource.findById(taskId)?.let { it
+                binding.tilTittle.text = it.tittle
+                binding.date.text = it.date
+                binding.tilHour.text = it.hour
+            }
+        }
 
         insertListeners()
     }
@@ -33,5 +48,40 @@ class AddTaskActivity : AppCompatActivity() {
 
             datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
         }
+
+        binding.tilHour.editText?.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+
+            timePicker.addOnPositiveButtonClickListener {
+                val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+
+                binding.tilHour.text = "$hour:$minute"
+            }
+
+            timePicker.show(supportFragmentManager, null)
+        }
+
+        binding.btnCancel.setOnClickListener {
+            finish()
+        }
+
+        binding.btnCriarTask.setOnClickListener {
+            val task = Task(
+                title = binding.tilTittle.text,
+                date = binding.date.text,
+                hour = binding.tilHour.text,
+                id = intent.getIntExtra(TASK_ID, 0)
+            )
+            TaskDataSource.insertTask(task)
+            setResult(RESULT_OK)
+            finish()
+        }
+    }
+
+    companion object {
+        const val TASK_ID = "task_id"
     }
 }
